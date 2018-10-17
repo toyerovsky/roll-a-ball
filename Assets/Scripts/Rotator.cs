c96c1630290440415290207ef6c337c5b902bb94
Przemysław P <toyerek@gmail.com>
2018-10-17 13:25:54 +0200

    Add Rotator script and collectables

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Rotator : MonoBehaviour
+    {
+        public void Update()
+        {
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        }
+    }
+}
